74e23df8905da6f5c4e0aa24fe58bc94cb4c226c
aksingh9263 <aksingh9263@gmail.com>
2024-06-06 12:47:39 +0530

    updated inference script

--- inference/checkpoint_inference.py
+++ inference/checkpoint_inference.py
@@ -1,11 +1,29 @@
-
-
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-olmo = AutoModelForCausalLM.from_pretrained("no_exist/checkpoints/OLMo-gemma-1.2b/step63-unsharded")
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 
-message = ["Language modeling is "]
-inputs = tokenizer(message, return_tensors='pt', return_token_type_ids=False)
-response = olmo.generate(**inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
-print(tokenizer.batch_decode(response, skip_special_tokens=True)[0])
+class CustomOlmo:
+    def __init__(self, model_path, tokenizer_path):
+        self.model = AutoModelForCausalLM.from_pretrained(model_path)
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+    def generate_text(self, input_text, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95):
+        inputs = self.tokenizer([input_text], return_tensors='pt', return_token_type_ids=False)
+        outputs = self.model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=do_sample, top_k=top_k,
+                                      top_p=top_p)
+        return self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
+
+if __name__ == '__main__':
+    model_path = "no_exist/checkpoints/OLMo-gemma-1.2b/step63-unsharded"
+    tokenizer_path = "google/gemma-2b-it"
+
+    lm = CustomOlmo(model_path, tokenizer_path)
+
+    while True:
+        input_text = input("Enter a prompt (type 'exit' to quit): ")
+        if input_text.lower() == 'exit':
+            print("Exiting...")
+            break
+        generated_text = lm.generate_text(input_text)
+        print(generated_text)
