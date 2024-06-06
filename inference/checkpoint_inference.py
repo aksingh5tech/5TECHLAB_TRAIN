391b32cae72271ce3e8c8aabd310345ebb4ab67d
aksingh9263 <aksingh9263@gmail.com>
2024-06-06 19:46:09 +0530

    hot fix

--- inference/checkpoint_inference.py
+++ inference/checkpoint_inference.py
@@ -19,7 +19,7 @@ class LanguageModel:
             yield self.tokenizer.decode(output, skip_special_tokens=True)
 
     def text_generation_pipeline(self,input_text):
-        olmo_pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer)
+        olmo_pipe = pipeline("text-generation", model=self.model, tokenizer=self.tokenizer, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
         return olmo_pipe(input_text)
 
 def main(checkpoint_dir, tokenizer):
