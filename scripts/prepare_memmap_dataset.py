324cbe75fc4ae65737b79504e3ab13813ed3978d
aksingh5tech <ankit.singh@5techlab.com>
2024-11-13 15:45:46 +0530

    5tech tokenizer

--- scripts/prepare_memmap_dataset.py
+++ scripts/prepare_memmap_dataset.py
@@ -351,7 +351,7 @@ def make_source_and_target(
     "tokenizer_id",
     type=str,
     help="Name of path of a pretrained tokenizer",
-    default="HuggingFaceTB/SmolLM2-135M",
+    default="allenai/eleuther-ai-gpt-neox-20b-pii-special",
 )
 @click.option("--dtype", "dtype_str", default="uint16")
 @click.option("--validate/--no-validate", default=False)
